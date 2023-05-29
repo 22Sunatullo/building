2b8dd4822341fb25995e4f7bd06a51756be637b6
22Sunatullo <muslimamuminova6969@gmail.com>
2023-05-29 17:01:39 +0500

    test

---
+++ src/app/components/screens/headers/Headers.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react"
+
+export const Headers:FC = () => {
+  return (
+    <div>
+       <h1>Header</h1>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Headers } from "./components/screens/headers/Headers";
+
+export default function Home() {
+  return (
+   <div>
+    <Headers/>
+   </div>
+  )
+}
