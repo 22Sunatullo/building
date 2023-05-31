ccbe32b9b10647d3a2f9380d265ae24562a99c41
22Sunatullo <muslimamuminova6969@gmail.com>
2023-05-31 10:30:36 +0500

    scss commit

--- src/app/components/screens/headers/Headers.tsx
+++ src/app/components/screens/headers/Headers.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react"
 
+import styles from './Headers.module.css'
+
 export const Headers:FC = () => {
   return (
-    <div>
-       <h1>Header</h1>
-    </div>
+    <header>
+       <div className="container">
+           <nav className="nav        ">
+            <a className="nav-link active" aria-current="page" href="#">Active</a>
+            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+           </nav>
+       </div>
+    </header>
   )
 }
+ 
