cfad615273fd0458b4fceb7965aa20201dbd9c62
22Sunatullo <muslimamuminova6969@gmail.com>
2023-06-08 18:26:26 +0500

    button

--- src/components/screens/NewsContent/NewsContent.tsx
+++ src/components/screens/NewsContent/NewsContent.tsx
@@ -1,13 +1,15 @@
-import { NewsCard } from "./NewsCard/NewsCard"
 import { NewsMap } from "./NewsMap/NewsMap"
 import { NewsTitle } from "./NewsTitle/NewsTitle"
-
+import style from './NewsContent.module.scss'
 
 export const NewsContent = () => {
   return (
     <div>
         <NewsTitle/>
         <NewsMap/>
+        <div className={style.buttonS}>
+          <button>Все новости</button>
+        </div>
     </div>
   )
 }
